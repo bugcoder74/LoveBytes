fbc9ea0c9993ca7c7a2109a9ca2c24d8fdd235a6
BugCoder <hdndcomputers6446@gmail.com>
2026-06-21 11:30:20 +0530

    Chore : Fresh generated Project

---
+++ src/main/java/com/bugcoder/LoveBytes/LoveBytesApplication.java
@@ -0,0 +1,13 @@
+package com.bugcoder.LoveBytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveBytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveBytesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bugcoder/LoveBytes/LoveBytesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugcoder.LoveBytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveBytesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
